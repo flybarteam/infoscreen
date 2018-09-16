c69503d9ed6868714ac75fe82a55a240080eaf73
hans christian aasen <hans_christian92@hotmail.com>
2018-09-16 18:10:19 +0200

    py

--- testrange.py
+++ testrange.py
@@ -4,7 +4,7 @@ import scipy
 OuterRadius = 0.0125
 InnerRadius = 0.0005
 
-SolundirLAT = 60.905908
+SolundirLAT = 60.918408
 SolundirLON = 4.901205
 
 MjomnaOuterLAT1 = 60.918404 - OuterRadius
@@ -12,15 +12,25 @@ MjomnaOuterLON1 = 4.901205 - OuterRadius
 MjomnaOuterLAT2 = 60.918404 + OuterRadius
 MjomnaOuterLON2 = 4.901205 + OuterRadius
 
-#MjomnaOuterRingLAT = np.linspace(MjomnaOuterLAT1, MjomnaOuterLAT2, 100000001)
-#MjomnaOuterRingLON = np.linspace(MjomnaOuterLON1, MjomnaOuterLON2, 100000001)
+MjomnaInnerLAT1 = 60.918404 - InnerRadius
+MjomnaInnerLON1 = 4.901205 - InnerRadius
+MjomnaInnerLAT2 = 60.918404 + OuterRadius
+MjomnaInnerLON2 = 4.901205 + OuterRadius
 
 MjomnaOuterRingLAT = scipy.arange(MjomnaOuterLAT1, MjomnaOuterLAT2, 0.000001)
 MjomnaOuterRingLAT = scipy.around(MjomnaOuterRingLAT, 6)
 
+MjomnaOuterRingLON = scipy.arange(MjomnaOuterLON1, MjomnaOuterLON2, 0.000001)
+MjomnaOuterRingLON = scipy.around(MjomnaOuterRingLON, 6)
 
-
-
-
-if SolundirLAT in MjomnaOuterRingLAT:
+if SolundirLAT in MjomnaOuterRingLAT and SolundirLON in MjomnaOuterRingLON:
     print('Solundir er i ytre ring')
+
+MjomnaInnerRingLAT = scipy.arange(MjomnaInnerLAT1, MjomnaInnerLAT2, 0.000001)
+MjomnaInnerRingLAT = scipy.around(MjomnaInnerRingLAT, 6)
+
+MjomnaInnerRingLON = scipy.arange(MjomnaInnerLON1, MjomnaInnerLON2, 0.000001)
+MjomnaInnerRingLON = scipy.around(MjomnaInnerRingLON, 6)
+
+if SolundirLAT in MjomnaInnerRingLAT and SolundirLON in MjomnaInnerRingLON:
+    print('Solundir er i indre ring')
